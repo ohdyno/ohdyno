97aa71a52867a8795a96c4d336bdb08377b4d183
Xing Zhou <xing@xingzhou.me>
2022-04-22 23:07:17 -0400

    feat: add /musings

---
+++ musings.11ty.js
@@ -0,0 +1,18 @@
+exports.data = {
+    title: "Musings"
+}
+
+function listMusings(data) {
+    return data.collections.musings ? `
+<ul>
+  ${data.collections.musings.map(post => `<li><a href="${post.url}">${post.data.title}</a></li>`).join("\n")}
+</ul>
+` : `</>`;
+}
+
+exports.render = function (data) {
+    return `
+<h1>${data.title}</h1>
+<p>Just a place to share some thoughts...</p>
+${listMusings(data)}`;
+}
