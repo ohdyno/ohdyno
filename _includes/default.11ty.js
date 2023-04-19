c5dadfa73aa63181ebb900513082b12be12e4c31
Xing Zhou <xing@xingzhou.me>
2023-04-19 11:43:19 -0400

    fix: close script tag

--- _includes/default.11ty.js
+++ _includes/default.11ty.js
@@ -17,7 +17,7 @@ exports.render = function (data) {
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
         <link rel="manifest" href="/site.webmanifest">
-        <script async src="/js/hello-world.js"/>
+        <script async src="/js/hello-world.js"></script>
       </head>
       <body class="site">
         ${this.nav(data.page)}
